30425bdc7b0ce7b640f0e07dd34c5e7ee8a8fbd9
Nevedhasubramani12 <154915117+Nevedhasubramani12@users.noreply.github.com>
2024-07-15 09:18:00 +0530

    Create 17.java

---
+++ 17.java
@@ -0,0 +1,22 @@
+//17.Count the word
+
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine().trim();  
+        
+        String[] words = input.split("\\s+");
+        
+        
+        int wordCount = words.length;
+        
+  
+        System.out.println("Number of words: " + wordCount);
+        
+        scanner.close();
+    }
+}
